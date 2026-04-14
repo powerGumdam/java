37bdbd29326a5152324045465a5ab20825a7558d
powerGumdam <kimjungjin710@gmail.com>
2026-04-14 23:48:51 +0900

    0414

---
+++ src/operater/Operater2.java
@@ -0,0 +1,25 @@
+package operater;
+
+public class Operater2 {
+
+    static void main() {
+
+        String result1 = "hello" + "world";
+        System.out.println(result1);
+
+        String s1 = "string1";
+        String s2 = "string2";
+        String result2 = s1 + s2;
+        System.out.println(result2);
+
+        String result3 = "a + b = " + 10;
+        System.out.println(result3);
+
+        int num = 20;
+        String str = "a + b = ";
+        String result4 = str + num;
+        System.out.println(result4);
+
+        
+    }
+}
